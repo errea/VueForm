f0b584bfbea729068c6d93a1edb626c419696f20
errea <erreakezy@gmail.com>
2022-01-18 16:22:51 +0100

    Add SignupForm component to setup form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SignupForm />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import SignupForm from './components/SignupForm';
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SignupForm,
   },
 };
 </script>
@@ -22,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SignupForm",
+  components: {},
+};
+// eslint-disable-next-line vue/comment-directive
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+label {
+  color: aaa;
+  display: inline-block;
+  margin: 25px 25px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+</style>
