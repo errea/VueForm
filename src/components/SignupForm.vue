1402b06d54650c71442f3dd7b411f4bebdf02cf2
errea <erreakezy@gmail.com>
2022-01-18 17:32:35 +0100

    Add SignupForm component

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,7 +1,15 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required />
+    <input type="email" required v-model="email" />
+
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
 </template>
 
@@ -9,6 +17,15 @@
 export default {
   name: "SignupForm",
   components: {},
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      terms: false,
+      tempSkills: "",
+    };
+  },
 };
 // eslint-disable-next-line vue/comment-directive
 </script>
@@ -24,7 +41,7 @@ form {
 }
 
 label {
-  color: aaa;
+  color: #aaa;
   display: inline-block;
   margin: 25px 25px;
   font-size: 0.6rem;
@@ -33,7 +50,8 @@ label {
   font-weight: bold;
 }
 
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -41,5 +59,12 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
